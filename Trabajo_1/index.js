b6b7745e33a291596c3c02852f33daa577efd631
Fede <fleonmendza@gmail.com>
2021-02-26 00:12:08 -0600

    Sistema web

--- Trabajo_1/index.js
+++ Trabajo_1/index.js
@@ -1,18 +1,21 @@
+
 var Http = require( 'http' );
 var server = Http.createServer(function(request,response){
+
     console.log('Alguien entró... 1,2,3');
     console.log(request.url);
-    console.log(JSON.stringify(request));
+
     response.writeHead(200,"Content-Type:text/html")
 
-    if(request.metod =='GET'){
-    response.write("<style> h1 {color:blue}</style>");
-    response.write("Fola ico fes aragon, la mejor carrera");
+    if(request.method =='GET'){
+      response.write("<style> h1 {color:blue}</style>");
+      response.write("Fola ico fes aragon, la mejor carrera");
 
-   }else {
-    response.write("no es get");
-  }
-  response.end();
+    }else {
+      response.write("no es get");
+    }
+
+    response.end();
 
 });
 
