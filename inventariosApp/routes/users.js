d75206ccd12c7685e493a094011b15db0e5b4fa2
Fede <fleonmendza@gmail.com>
2021-03-12 13:10:29 -0600

    clase 12 marzo

--- inventariosApp/routes/users.js
+++ inventariosApp/routes/users.js
@@ -4,7 +4,7 @@ var usuario = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render("frmLogin", {});
 });
 
 
@@ -14,10 +14,13 @@ router.post('/login', (req , res , next)=>{
 
   usuario.login(req.body.email, req.body.passwd,( e , d )=>{
     if(d){
-      res.send('Login correcto');
+      //res.send('Login correcto');
       ses=req.session;
       console.log(ses.id);
-      
+      ses.userdata = d;
+      console.log(ses);
+      res.redirect('/');
+
     }else{
       res.json(e);
     }
@@ -25,6 +28,14 @@ router.post('/login', (req , res , next)=>{
 
 });
 
-
+router.get('/logout',(req,res,next)=>{
+  req.session.destroy((falla)=>{
+    if (falla) {
+      res.send(501,"Error")
+    } else {
+      res.redirect('/');
+    }
+  });
+});
 
 module.exports = router;
